86211849531f42969cf81d9647eea133560195dd
MiguelCanais <miguelmariacanais@gmail.com>
2024-12-11 19:39:50 +0000

    added matplotlib

--- dados_celulas.py
+++ dados_celulas.py
@@ -1,84 +1,204 @@
-from openpyxl import load_workbook
+def obtemDadosRelatorio(sheet):
+    return {
+        "pedidos": {
+            "prod1": sheet["AA12"].value,
+            "prod2": sheet["AC12"].value,
+            "prod3": sheet["AE12"].value,
+        },
 
-class DadosProduto:
-    def __init__(self, fileName):
-        wb = load_workbook(f"./relatorios/{fileName}.xlsx")
-        self.sheet = wb["Excel_1"]
+        "produzidos": {
+            "prod1": sheet["AA13"].value,
+            "prod2": sheet["AC13"].value,
+            "prod3": sheet["AE13"].value,
+        },
 
-        self.produzidos = {
-            "produto1": self.sheet["AA13"].value,
-            "produto2": self.sheet["AC13"].value,
-            "produto3": self.sheet["AE13"].value,
-        }
+        "rejeitados": {
+            "prod1": sheet["AA14"].value,
+            "prod2": sheet["AC14"].value,
+            "prod3": sheet["AE14"].value,
+        },
 
-        # A quantidade de produtos que a empresa entregou
-        self.entregas = {
-            "produto1": {
-                "ue":        self.sheet["AA18"].value,
-                "nafta":     self.sheet["AA19"].value,
-                "internet":  self.sheet["AA20"].value,
+        "destruidos": {
+            "prod1": sheet["AA15"].value,
+            "prod2": sheet["AC15"].value,
+            "prod3": sheet["AE15"].value,
+        },
+
+        "reparados": {
+            "prod1": sheet["AA45"].value,
+            "prod2": sheet["AC45"].value,
+            "prod3": sheet["AE45"].value,
+        },
+
+        "reclamacoesWebsite": {
+            "prod1": sheet["AA48"].value,
+            "prod2": sheet["AC48"].value,
+            "prod3": sheet["AE48"].value,
+        },
+
+        "entregas": {
+            "prod1": {
+                "ue":        sheet["AA18"].value,
+                "nafta":     sheet["AA19"].value,
+                "internet":  sheet["AA20"].value,
             },
-            "produto2": {
-                "ue":        self.sheet["AC18"].value,
-                "nafta":     self.sheet["AC19"].value,
-                "internet":  self.sheet["AC20"].value,
+            "prod2": {
+                "ue":        sheet["AC18"].value,
+                "nafta":     sheet["AC19"].value,
+                "internet":  sheet["AC20"].value,
             },
-            "produto3": {
-                "ue":        self.sheet["AE18"].value,
-                "nafta":     self.sheet["AE19"].value,
-                "internet":  self.sheet["AE20"].value,
+            "prod3": {
+                "ue":        sheet["AE18"].value,
+                "nafta":     sheet["AE19"].value,
+                "internet":  sheet["AE20"].value,
             },
-        }
+        },
 
         # A quantidade de produtos requesitados
-        self.encomendas = {
-            "produto1": {
-                "ue":        self.sheet["AA23"].value,
-                "nafta":     self.sheet["AA24"].value,
-                "internet":  self.sheet["AA25"].value,
+        "encomendas": {
+            "prod1": {
+                "ue":        sheet["AA23"].value,
+                "nafta":     sheet["AA24"].value,
+                "internet":  sheet["AA25"].value,
             },
-            "produto2": {
-                "ue":        self.sheet["AC23"].value,
-                "nafta":     self.sheet["AC24"].value,
-                "internet":  self.sheet["AC25"].value,
+            "prod2": {
+                "ue":        sheet["AC23"].value,
+                "nafta":     sheet["AC24"].value,
+                "internet":  sheet["AC25"].value,
             },
-            "produto3": {
-                "ue":        self.sheet["AE23"].value,
-                "nafta":     self.sheet["AE24"].value,
-                "internet":  self.sheet["AE25"].value,
+            "prod3": {
+                "ue":        sheet["AE23"].value,
+                "nafta":     sheet["AE24"].value,
+                "internet":  sheet["AE25"].value,
             },
-        }
+        },
 
         # A quantidade de vendas
-        self.vendas = {
-            "produto1": {
-                "ue":        self.sheet["AA28"].value,
-                "nafta":     self.sheet["AA29"].value,
-                "internet":  self.sheet["AA30"].value,
+        "vendas": {
+            "prod1": {
+                "ue":        sheet["AA28"].value,
+                "nafta":     sheet["AA29"].value,
+                "internet":  sheet["AA30"].value,
             },
-            "produto2": {
-                "ue":        self.sheet["AC28"].value,
-                "nafta":     self.sheet["AC29"].value,
-                "internet":  self.sheet["AC30"].value,
+            "prod2": {
+                "ue":        sheet["AC28"].value,
+                "nafta":     sheet["AC29"].value,
+                "internet":  sheet["AC30"].value,
             },
-            "produto3": {
-                "ue":        self.sheet["AE28"].value,
-                "nafta":     self.sheet["AE29"].value,
-                "internet":  self.sheet["AE30"].value,
+            "prod3": {
+                "ue":        sheet["AE28"].value,
+                "nafta":     sheet["AE29"].value,
+                "internet":  sheet["AE30"].value,
             },
-        }
+        },
 
-        self.encomendasAtraso = {
-            "produto1": {
-                "ue":        self.sheet["AA33"].value,
-                "nafta":     self.sheet["AA34"].value,
+        "encomendasAtraso": {
+            "prod1": {
+                "ue":        sheet["AA33"].value,
+                "nafta":     sheet["AA34"].value,
             },
-            "produto2": {
-                "ue":        self.sheet["AC33"].value,
-                "nafta":     self.sheet["AC34"].value,
+            "prod2": {
+                "ue":        sheet["AC33"].value,
+                "nafta":     sheet["AC34"].value,
             },
-            "produto3": {
-                "ue":        self.sheet["AE33"].value,
-                "nafta":     self.sheet["AE34"].value,
+            "prod3": {
+                "ue":        sheet["AE33"].value,
+                "nafta":     sheet["AE34"].value,
+            },
+        },
+
+        "inventario": {
+            "prod1": {
+                "ue":        sheet["AA37"].value,
+                "nafta":     sheet["AA38"].value,
+                "internet":  sheet["AA39"].value,
+            },
+            "prod2": {
+                "ue":        sheet["AC37"].value,
+                "nafta":     sheet["AC38"].value,
+                "internet":  sheet["AC39"].value,
+            },
+            "prod3": {
+                "ue":        sheet["AE37"].value,
+                "nafta":     sheet["AE38"].value,
+                "internet":  sheet["AE39"].value,
+            },
+        },
+
+        "montagem": {
+            "horasDisponiveis": sheet["U14"].value,
+            "absentismo":       sheet["U15"].value,
+            "horasUtilizadas":  sheet["U16"].value,
+        },
+
+        "maquinacao": {
+            "horasDisponiveis": sheet["U20"].value, 
+            "tempoParalizacao": sheet["U21"].value, 
+            "horasConservacao": sheet["U22"].value, 
+            "horasUtilizadas": sheet["U23"].value, 
+            "eficienciaMaquinas": sheet["U24"].value, 
+        },
+
+        "materiaPrima": {
+            "inventarioInicial": sheet["U27"].value,
+            "comprasUltimoTrimestre": sheet["U28"].value,
+            "destruida": sheet["U30"].value,
+            "utilizada": sheet["U31"].value,
+            "inventarioArmazem": sheet["U32"].value,
+        },
+
+        "recursosHumanos": {
+            "disponivel": {
+                "especializados": sheet["T40"].value,
+                "naoEspecializados": sheet["U40"].value,
+            },
+            "recrutado": {
+                "especializados": sheet["T41"].value,
+                "naoEspecializados": sheet["U41"].value,
+            },
+            "formados": {
+                "especializados": sheet["T42"].value,
+                "naoEspecializados": sheet["U42"].value,
+            },
+            "despedidos": {
+                "especializados": sheet["T43"].value,
+                "naoEspecializados": sheet["U43"].value,
+            },
+            "abandono": {
+                "especializados": sheet["T44"].value,
+                "naoEspecializados": sheet["U44"].value,
+            },
+            "disponivelProximoTrimestre": {
+                "especializados": sheet["T45"].value,
+                "naoEspecializados": sheet["U45"].value,
+            },
+        },
+
+        "distribuidores": {
+            "disponivel": {
+                "ue": sheet["S48"].value,
+                "nafta": sheet["T48"].value,
+                "internet": sheet["U48"].value,
+            },
+            "perdidos": {
+                "ue": sheet["S49"].value,
+                "nafta": sheet["T49"].value,
+                "internet": sheet["U49"].value,
+            },
+            "rescindidos": {
+                "ue": sheet["S50"].value,
+                "nafta": sheet["T50"].value,
+                "internet": sheet["U50"].value,
+            },
+            "novos": {
+                "ue": sheet["S51"].value,
+                "nafta": sheet["T51"].value,
+                "internet": sheet["U51"].value,
+            },
+            "disponivelProximoTrimestre": {
+                "ue": sheet["S52"].value,
+                "nafta": sheet["T52"].value,
+                "internet": sheet["U52"].value,
             },
         }
+    }
--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
-def calcula(tipo,dados):
-    tipo = tipo.lower()
+from openpyxl import load_workbook
+from dados_celulas import obtemDadosRelatorio 
+
+def calculaValor(tipo,dados):
     total = 0
 
     # Calcula Total
     if tipo == "total":
-        for produto,val in dados.items():
+        for k,val in dados.items():
             if isinstance(val,dict):
-                total += calcula(produto,dados)
+                total += calculaValor(k,dados)
             else:
                 total += val
 
     # Calcula Produto
-    elif tipo.startswith("produto"):
+    elif tipo.startswith("prod"):
         if isinstance(dados[tipo],dict):
             for vendasMercado in dados[tipo].values():
                 total += vendasMercado
@@ -24,3 +26,40 @@ def calcula(tipo,dados):
             total += produto[tipo]
 
     return total
+
+def loadSheet(fileName):
+    wb = load_workbook(f"./relatorios/{fileName}.xlsx")
+    sheet = wb["Excel_1"]
+    return sheet
+
+
+def obtemValor(tipo: str, nomeRelatorio: str) -> int|float:
+    '''
+    Exemplos de tipo:
+        "vendas" - Calcula o valor de todas as vendas
+        "vendas:prod1" - Calcula o valor de todas as vendas do produto
+        "vendas:ue" - 
+        "vendas:prod1:ue"
+    '''
+    sheet = loadSheet(nomeRelatorio)
+    dados = obtemDadosRelatorio(sheet)
+
+    l = tipo.split(':')
+    s = l[0]
+
+    valores = dados[s]
+
+    if len(l) == 1:
+        return calculaValor("total",valores)
+    elif len(l) == 2:
+        return calculaValor("")
+    else:
+        s1 = l[1]
+        s2 = l[2]
+        return valores[s1][s2]
+    
+
+
+
+    
+
